a04d8447d053b030ab22508987869ec8d2ebdd20
ArnoldBrown <arnoldbrown033@gmail.com>
2024-12-04 13:12:54 +0530

    bug fix

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -42,7 +42,7 @@ class _QuizState extends State<Quiz> {
   void restartQuiz() {
     setState(() {
       selectedAnswers = [];
-      activeScreen = 'questions-screens';
+      activeScreen = 'questions-screen';
     });
   }
 
